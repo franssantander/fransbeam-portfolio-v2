334373b7f8f962395d3e6e9d5b63fa64539182eb
francesantander <santanderfrancisbeam@gmail.com>
2025-03-12 07:33:13 +0800

    installed dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react";
+import Unfonts from 'unplugin-fonts/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: "Geist",
+            // src: path.resolve(__dirname, "src/assets/fonts/geist/*.woff2"),
+            src: './src/assets/fonts/geist/*.woff2',
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 8000,
+    open: true,
+  },
+});
